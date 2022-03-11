571e2e593ddae658530ef779ecbeebe022507869
Fernando Ayrosa Vezzali <fvezzali@questrade.com>
2022-03-11 14:26:12 -0300

    updating before project

---
+++ src/before/UnitTestCourse.Test/API/TodoApplication.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace UnitTestCourse.Test
+{
+    public class TodoApplication : WebApplicationFactory<Todo>
+    {
+        protected override IHost CreateHost(IHostBuilder builder)
+        {
+            var factory = new ConnectionFactory();
+
+            var context = factory.CreateContextForSQLite();
+            
+            builder.ConfigureServices(services =>
+            {
+                services.AddScoped(sp =>
+                {
+                    return context;
+                });
+            });
+
+            return base.CreateHost(builder);
+        }
+    }
+}
---
+++ src/before/UnitTestCourse.Test/API/TodoContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace UnitTestCourse.Test
+{
+    public partial class TodoContext : DbContext
+    {
+        public TodoContext() { }
+
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options) { }
+
+        public virtual DbSet<Todo> Todos { get; set; }
+    }
+}

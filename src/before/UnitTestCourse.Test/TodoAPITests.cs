2a3bd9d7b8b355b7e69f63844886c100b5885866
Fernando Ayrosa Vezzali <fvezzali@questrade.com>
2022-03-21 13:55:23 -0300

    removing implementation

--- src/before/UnitTestCourse.Test/TodoAPITests.cs
+++ src/before/UnitTestCourse.Test/TodoAPITests.cs
@@ -7,73 +7,31 @@ using System.Net;
 
 namespace UnitTestCourse.Test
 {
-
+    // todo 12 API Testing
     public class TodoApiTests
     {
         [Fact]
         public async Task ShouldReturnHttpStatusCodeCreated()
         {
-            // Arrange
-            await using var application = new TodoApplication();
 
-            var client = application.CreateClient();
-
-            // Act
-            var response = await client.PostAsJsonAsync("/todos", new Todo { Title = "I want to do this thing tomorrow" });
-
-            // Assert
-            Assert.Equal(HttpStatusCode.Created, response.StatusCode);
         }
 
         [Fact]
         public async Task ShouldFindTodoByTitle()
         {
-            // Arrange
-            await using var application = new TodoApplication();
 
-            var client = application.CreateClient();
-
-            // Act
-            var response = await client.PostAsJsonAsync("/todos", new Todo { Title = "I want to do this thing tomorrow" });
-
-            var todos = await client.GetFromJsonAsync<List<Todo>>("/todos");
-
-            // Assert
-            Assert.Equal("I want to do this thing tomorrow", todos!.LastOrDefault()!.Title);
         }
 
         [Fact]
         public async Task ShouldFindOnlyOneItem()
         {
-            // Arrange
-            await using var application = new TodoApplication();
 
-            var client = application.CreateClient();
-
-            // Act
-            await client.DeleteAsync("/todos/delete-all");
-
-            var response = await client.PostAsJsonAsync("/todos", new Todo { Title = "I want to do this thing tomorrow" });
-
-            var todos = await client.GetFromJsonAsync<List<Todo>>("/todos");
-
-            // Assert
-            Assert.Single(todos);
         }
 
         [Fact]
         public async Task ShouldDelete()
         {
-            // Arrange
-            await using var application = new TodoApplication();
 
-            var client = application.CreateClient();
-
-            // Act
-            var response = await client.DeleteAsync("/todos/delete-all");
-
-            // Assert
-            Assert.True(response.IsSuccessStatusCode);
         }
     }
 }

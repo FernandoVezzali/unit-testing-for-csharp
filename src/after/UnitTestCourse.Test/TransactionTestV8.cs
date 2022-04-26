2c5b134da0bf311daa093230f1cd4033804249d3
fvezzali <fernando.vezzali@vanco.com>
2022-04-25 23:09:10 -0300

    skipping tests

---
+++ src/after/UnitTestCourse.Test/TransactionTestV8.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+// todo 13 xUnit: Skip
+namespace UnitTestCourse.Test
+{
+    public class TransactionTestV8
+    {
+        [Fact(Skip = "skipping the test")]
+        public void ShouldReturnAmount200()
+        {
+            Assert.Equal(1, 2);
+        }
+    }
+}
---
+++ src/before/UnitTestCourse.Test/TransactionTestV8.cs
@@ -0,0 +1,12 @@
+﻿// todo 13 xUnit: Skip
+namespace UnitTestCourse.Test
+{
+    public class TransactionTestV8
+    {
+        //[Fact]
+        //public void ShouldReturnAmount200()
+        //{
+        //    Assert.Equal(1, 2);
+        //}
+    }
+}
